24202a13a003f343c48c0a4cc9f478df94385d47
Tri Khuu <khatrikhuu@gmail.com>
2025-12-10 17:39:44 +0700

    Added feature extractor to map

--- lidar/slam/map/map.cpp
+++ lidar/slam/map/map.cpp
@@ -5,14 +5,19 @@ namespace slam {
 namespace map {
 
 absl::StatusOr<std::unique_ptr<Map>>
-Map::Create(std::unique_ptr<IDownSample<pcl::PointXYZI>> down_sampler) {
+Map::Create(std::unique_ptr<IDownSample<pcl::PointXYZI>> down_sampler, std::unique_ptr<IFeatureExtractor<pcl::PointXYZI>> feature_extractor) {
   if (down_sampler == nullptr) {
     return absl::InvalidArgumentError("down_sampler is null");
+  }
+
+  if (feature_extractor == nullptr) {
+    return absl::InvalidArgumentError("feature_extractor is null");
   }
 
   std::unique_ptr<Map> map(new Map());
 
   map->down_sampler_ = std::move(down_sampler);
+  map->feature_extractor_ = std::move(feature_extractor);
 
   return map;
 }
--- lidar/slam/map/map.h
+++ lidar/slam/map/map.h
@@ -5,8 +5,10 @@
 #include "pcl/point_types.h"
 
 #include "lidar/slam/down_sample/down_sample.h"
+#include "lidar/slam/feature_extractor/feature_extractor.h"
 
 using lidar::slam::down_sample::IDownSample;
+using lidar::slam::feature_extractor::IFeatureExtractor;
 
 namespace lidar {
 namespace slam {
@@ -15,12 +17,14 @@ namespace map {
 class Map {
 public:
   static absl::StatusOr<std::unique_ptr<Map>>
-  Create(std::unique_ptr<IDownSample<pcl::PointXYZI>> down_sampler);
+  Create(std::unique_ptr<IDownSample<pcl::PointXYZI>> down_sampler,
+         std::unique_ptr<IFeatureExtractor<pcl::PointXYZI>> feature_extractor);
 
 private:
   Map() = default;
 
   std::unique_ptr<IDownSample<pcl::PointXYZI>> down_sampler_;
+  std::unique_ptr<IFeatureExtractor<pcl::PointXYZI>> feature_extractor_;
 };
 
 } // namespace map
