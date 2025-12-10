7248f39ee579cada877b09555c2046e1e5c95d0b
Tri Khuu <khatrikhuu@gmail.com>
2025-12-10 17:21:08 +0700

    Added constructor for VoxelGrid

--- lidar/slam/down_sample/voxel_grid_params.h
+++ lidar/slam/down_sample/voxel_grid_params.h
@@ -11,6 +11,9 @@ struct VoxelGridParams : public IDownSampleParams {
   float leaf_size_x;
   float leaf_size_y;
   float leaf_size_z;
+
+  VoxelGridParams(float lsize_x, float lsize_y, float lsize_z)
+      : leaf_size_x(lsize_x), leaf_size_y(lsize_y), leaf_size_z(lsize_z) {}
 };
 
 } // namespace down_sample
